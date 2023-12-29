caf07d381056315089a7f0a9d34a8573a266ba14
kelvinjchung <kchung1223@gmail.com>
2023-12-29 14:38:10 -0800

    places autocomplete search focus/blur functionality + Places API error handling

--- src/components/elements/PlacesSearch.tsx
+++ src/components/elements/PlacesSearch.tsx
@@ -10,10 +10,10 @@ interface PlacesSearchProps
 }
 
 const PlacesSearch = ({ placeholder, requestOptions }: PlacesSearchProps) => {
-  const [currIdx, setCurrIdx] = useState<number>(0);
-
+  // currIdx === null when listbox is not expanded
+  const [currIdx, setCurrIdx] = useState<number | null>(null);
   const {
-    suggestions: { status, data },
+    suggestions: { loading, status, data },
     value,
     setValue,
   } = usePlacesAutocomplete({
@@ -22,7 +22,8 @@ const PlacesSearch = ({ placeholder, requestOptions }: PlacesSearchProps) => {
     debounce: 300,
   });
 
-  console.log(`status: ${status}\n data:`, data);
+  // * FOR TESTING: DELETE LATER
+  console.log(`loading: ${loading}\nstatus: ${status}\n data:`, data);
   console.log(`currIdx: ${currIdx}`);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,11 +42,12 @@ const PlacesSearch = ({ placeholder, requestOptions }: PlacesSearchProps) => {
       case "ArrowUp":
         e.preventDefault();
         console.log("ArrowUp");
-        if (currIdx > 0) setCurrIdx(currIdx - 1);
+        if (currIdx !== null && currIdx > 0) setCurrIdx(currIdx - 1);
         break;
       case "ArrowDown":
         e.preventDefault();
-        if (currIdx < data.length - 1) setCurrIdx(currIdx + 1);
+        if (currIdx !== null && currIdx < data.length - 1)
+          setCurrIdx(currIdx + 1);
         console.log("ArrowDown");
         break;
       default:
@@ -53,8 +55,22 @@ const PlacesSearch = ({ placeholder, requestOptions }: PlacesSearchProps) => {
     }
   };
 
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (e.target.value !== "" && loading === false) setCurrIdx(0);
+  };
+
+  const handleBlur = () => {
+    setCurrIdx(null);
+    setValue("");
+  };
+
   const handlePointerMove = (idx: number) => () => {
     setCurrIdx(idx);
+  };
+
+  const handleSelect = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation;
+    e.preventDefault();
   };
 
   const suggestions = data.map(
@@ -68,9 +84,11 @@ const PlacesSearch = ({ placeholder, requestOptions }: PlacesSearchProps) => {
     ) => (
       <div
         key={place_id}
+        id={`placesSearch-listbox-option-${idx}`}
         className="aria-selected:bg-accent aria-selected:text-accent-foreground"
-        role="option"
         onPointerMove={handlePointerMove(idx)}
+        onClick={handleSelect}
+        role="option"
         aria-selected={currIdx === idx}
       >
         <span>{main_text}</span>
@@ -79,12 +97,33 @@ const PlacesSearch = ({ placeholder, requestOptions }: PlacesSearchProps) => {
     ),
   );
 
-  // TODO: add loading
-  const suggestionsLoading = <div role="progressbar">Loading...</div>;
-
-  // const suggestionsError =
-
-  const suggestionsNone = <div role="presentation">No results found.</div>;
+  const renderSuggestions = () => {
+    // TODO: use shadcn skeleton
+    if (loading) return <div role="progressbar">Loading...</div>;
+    else {
+      switch (status) {
+        case "ZERO_RESULTS":
+        case "NOT_FOUND":
+          return <div role="presentation">No results found.</div>;
+        case "INVALID_REQUEST":
+        case "UNKNOWN_ERROR":
+          return <div role="presentation">Error</div>;
+        case "OVER_QUERY_LIMIT":
+          return <div role="presentation">Over API query limit</div>;
+        case "OK":
+          // TODO: might need to make ids unique per listbox if multiple PlacesSearch components are on one page
+          return (
+            <div
+              role="listbox"
+              id={`placesSearch-listbox`}
+              onMouseDown={(e) => e.preventDefault()}
+            >
+              {suggestions}
+            </div>
+          );
+      }
+    }
+  };
 
   return (
     <div>
@@ -93,16 +132,22 @@ const PlacesSearch = ({ placeholder, requestOptions }: PlacesSearchProps) => {
         value={value}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         autoComplete="off"
         autoCorrect="off"
         spellCheck="false"
-        aria-autocomplete="list"
         role="combobox"
+        aria-autocomplete="list"
+        aria-expanded={currIdx !== null}
+        aria-controls="placesSearch-listbox"
+        aria-activedescendant={
+          currIdx !== null
+            ? `placesSearch-listbox-option-${currIdx}`
+            : undefined
+        }
       />
-      {status === "OK" && <div role="listbox">suggestions</div>}
-      {(status === "ZERO_RESULTS" || status === "NOT_FOUND") && suggestionsNone}
-      {status === "INVALID_REQUEST" || status === "UNKNOWN_ERROR"}
-      {status === "OVER_QUERY_LIMIT" && <div>Over query limit</div>}
+      {currIdx !== null && renderSuggestions()}
     </div>
   );
 };

32b9ff351204884de3b78c7472f05c42b46449a6
kelvinjchung <kchung1223@gmail.com>
2024-01-11 17:26:30 -0800

    update DateRangePicker for react-hook-form

--- src/components/elements/DateRangePicker.tsx
+++ src/components/elements/DateRangePicker.tsx
@@ -3,15 +3,13 @@
 import { cn } from "@/lib/utils";
 import { CalendarIcon } from "@radix-ui/react-icons";
 import { format } from "date-fns";
-import { useState } from "react";
 import { DateRange } from "react-day-picker";
 import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
-import { Input } from "../ui/input";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
-interface IDateRangePickerProps {
-  dateRange: DateRange | undefined;
+interface DateRangePickerProps {
+  dateRange: DateRange;
   setDateRange: React.Dispatch<React.SetStateAction<DateRange | undefined>>;
   dateFormat?: string;
 }
@@ -20,43 +18,45 @@ const DateRangePicker = ({
   dateRange,
   setDateRange,
   dateFormat = "LLL dd",
-}: IDateRangePickerProps) => {
+}: DateRangePickerProps) => {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          className={cn(
-            "justify-start font-normal",
-            !dateRange && "text-muted-foreground",
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {dateRange?.from ? (
-            dateRange.to ? (
-              <>
-                {format(dateRange.from, dateFormat)} -{" "}
-                {format(dateRange.to, dateFormat)}
-              </>
+    <div>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            className={cn(
+              "w-full justify-start font-normal",
+              !dateRange && "text-muted-foreground",
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {dateRange?.from ? (
+              dateRange.to ? (
+                <>
+                  {format(dateRange.from, dateFormat)} -{" "}
+                  {format(dateRange.to, dateFormat)}
+                </>
+              ) : (
+                format(dateRange.from, dateFormat)
+              )
             ) : (
-              format(dateRange.from, dateFormat)
-            )
-          ) : (
-            <span>Pick a date</span>
-          )}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent align="start" className="w-auto p-0">
-        <Calendar
-          initialFocus
-          mode="range"
-          defaultMonth={dateRange?.from}
-          selected={dateRange}
-          onSelect={setDateRange}
-          numberOfMonths={window.innerWidth > 768 ? 2 : 1}
-        />
-      </PopoverContent>
-    </Popover>
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent align="start" className="w-auto p-0">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={dateRange?.from}
+            selected={dateRange}
+            onSelect={setDateRange}
+            numberOfMonths={window.innerWidth > 768 ? 2 : 1}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 };
 

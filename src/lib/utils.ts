1cb6772e33fac7b0e0dfd6ba9743bec7b065886b
kelvinjchung <kchung1223@gmail.com>
2024-02-02 18:05:54 -0800

    util function to partition locations by day of trip

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,35 @@
-import { type ClassValue, clsx } from "clsx";
+import { Location } from "@prisma/client";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function partitionLocationsByDay(
+  locations: Location[],
+  numDays: number,
+) {
+  // initialize an array of length numDays + 1 with empty arrays
+  const initialArray = Array.from(Array(numDays + 1), () => [] as Location[]);
+
+  // locationsByDay[] is an array of length numDays + 1
+  // locationsByDay[x] is an array of locations with day === x
+  // locationsByDay[-1] is an array of locations with day === null
+  const locationsByDay = locations.reduce((acc, location) => {
+    const date = location.day;
+    if (date === null) {
+      if (!acc[numDays]) {
+        acc[numDays] = [];
+      }
+      acc[numDays].push(location);
+      return acc;
+    }
+    if (!acc[date]) {
+      acc[date] = [];
+    }
+    acc[date].push(location);
+    return acc;
+  }, initialArray);
+  return locationsByDay;
 }

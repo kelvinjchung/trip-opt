a8ec6031cd92ce7cbd895a208eede7c7b87e6c42
kelvinjchung <kchung1223@gmail.com>
2024-06-06 19:53:30 -0700

    setup drizzle + plan & location schemas

---
+++ server/drizzle.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/db/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+        host: process.env.POSTGRES_HOST,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+    },
+    tablesFilter: ["tripopt_*"]
+});
---
+++ server/src/db/schema.ts
@@ -0,0 +1,36 @@
+import { sql } from "drizzle-orm";
+import { boolean, date, decimal, pgTableCreator, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const createTable = pgTableCreator((name) => `tripopt_${name}`)
+
+export const plans = createTable(
+  "plan",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+    // userId: 
+    name: varchar("name", { length: 128 }).notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow(),
+    startDate: date("startDate").notNull(),
+    endDate: date("endDate").notNull(),
+    destination_placeId: varchar("destination_placeId", { length: 256 }).notNull(),
+    destination_name: varchar("destination_name", { length: 56 }).notNull(),
+    destination_lat: decimal("destination_lat", { precision: 9, scale: 6 }).notNull(),
+  },
+
+)
+
+export const locations = createTable(
+  "location",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: varchar("name", { length: 128 }).notNull(),
+    placeId: varchar("place_id", { length: 256 }).notNull(),
+    lat: decimal("lat", { precision: 9, scale: 6 }).notNull(),
+    lng: decimal("lat", { precision: 9, scale: 6 }).notNull(),
+    address: varchar("address", { length: 128 }).notNull(),
+    types: varchar("types", { length: 30 }).array().notNull().default(sql`ARRAY[]::varchar[]`),
+    dateTime: timestamp("dateTime"),
+    locked: boolean("locked").notNull().default(false),
+    planId: uuid("planId").references(() => plans.id).notNull(),
+  }
+)
